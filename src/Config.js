6757397c24ff86e4269da21099d0d9afdca731d9
Kai Wegner <kwnetzwelt@gmail.com>
2020-04-16 00:32:55 +0200

    added static assets path to config

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,8 @@
 module.exports = {
     apiEndpoint : "http://192.168.0.130:3030",
+    staticAssets : "http://192.168.0.130:3030/static/",
     locale : "de",
     dateTimeFormat:{ year: 'numeric', month: 'numeric', day: '2-digit' },
     dateTimeFormatMonthName:{month:'long', year:'numeric'},
-    toCurrencyValue :  value => value.toFixed(2) + " €"
+    toCurrencyValue :  value => value.toFixed(2) + " €" 
 }
