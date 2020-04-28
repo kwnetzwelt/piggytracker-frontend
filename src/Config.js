3f685810f9a10b0673af5101f5ce3df7359540e3
Kai Wegner <kwnetzwelt@gmail.com>
2020-04-29 00:42:51 +0200

    changed apiEndpoint and StaticAssets url in config

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 const Config =class {
 
     /** The API Endpoint (backend) to connect to */
-    static apiEndpoint = "http://192.168.0.191:3030";
+    static apiEndpoint = "https://api.piggytracker.org";
     /** The locale to use. Either something like "de" or "de_DE" is valid here. It is used for date formatting. */
     static locale = "de";
     /** Date Time format used in the entries list */
@@ -15,7 +15,7 @@ const Config =class {
     /** Currenty display according to your used currency. */
     static toCurrencyValue = value => value.toFixed(2) + "\u00a0€";
     /** The base url of all static assets like category, month and avatar images. */
-    static staticAssets = "http://192.168.0.191:3030/static/";
+    static staticAssets = "http://static.piggytracker.org/";
     /** Getters for static content urls*/
     static getCategoryUrl = (category) => this.staticAssets +"/categories/" + category.toLowerCase().replace(" ","-") + ".png";
     static getAvatarUrl = (fullName) => this.staticAssets + "/avatars/" + fullName.toLowerCase().replace(" ","-") + ".jpg";
