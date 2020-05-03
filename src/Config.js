e431f52fd93ec8eeaad36d5c68f5f15f701968a7
Kai Wegner <kwnetzwelt@gmail.com>
2020-05-03 19:45:36 +0200

    config changed reverted

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 const Config =class {
 
     /** The API Endpoint (backend) to connect to */
-    static apiEndpoint = "http://localhost:3030";
+    static apiEndpoint = "http://api.piggytracker.org";
     /** For almost all calls we will use this as a prefix route */
     static apiEndpointPrefixRoute = "/api/v1";
     /** The locale to use. Either something like "de" or "de_DE" is valid here. It is used for date formatting. */
@@ -17,7 +17,7 @@ const Config =class {
     /** Currenty display according to your used currency. */
     static toCurrencyValue = value => value.toFixed(2) + "\u00a0€";
     /** The base url of all static assets like category, month and avatar images. */
-    static staticAssets = "http://localhost:3030/static";
+    static staticAssets = "http://api.piggytracker.org/static";
     /** Getters for static content urls*/
     static getCategoryUrl = (category) => this.staticAssets +"/categories/" + category.toLowerCase().replace(" ","-") + ".png";
     static getAvatarUrl = (fullName) => this.staticAssets + "/avatars/" + fullName.toLowerCase().replace(" ","-");
